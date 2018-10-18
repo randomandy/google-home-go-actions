8f17be5de5c7a29591c89ff60505ab7e01e73293
Andy Ruland <aruland@gmail.com>
2018-10-18 14:50:40 +0800

    Returning random cocktail

--- dialogflow_api_v2/app.go
+++ dialogflow_api_v2/app.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 
 	// "github.com/davecgh/go-spew/spew"
+	"github.com/Depado/articles/code/dialogflow/cocktail"
 	"github.com/gin-gonic/gin"
 	df "github.com/leboncoin/dialogflow-go-webhook"
+	"github.com/sirupsen/logrus"
 )
 
 func webhook(c *gin.Context) {
@@ -26,12 +29,40 @@ func webhook(c *gin.Context) {
 		log.Println("Search action detected")
 		c.JSON(http.StatusOK, gin.H{})
 	case "random":
-		log.Println("Search action detected")
-		c.JSON(http.StatusOK, gin.H{})
+		log.Println("Random action detected")
+		random(c, dfr)
 	default:
 		log.Println("Unknown action")
 		c.AbortWithStatus(http.StatusNotFound)
 	}
+
+	c.JSON(http.StatusOK, gin.H{})
+}
+
+func search(c *gin.Context, dfr *df.Request) {
+
+}
+
+func random(c *gin.Context, dfr *df.Request) {
+	var err error
+	var d *cocktail.FullDrink
+
+	if d, err = cocktail.C.GetRandomDrink(); err != nil {
+		logrus.WithError(err).Error("Coudln't get random drink")
+		c.AbortWithStatus(http.StatusInternalServerError)
+		return
+	}
+	fmt.Println(d)
+
+	out := fmt.Sprintf("I found that cocktail : %s", d.StrDrink)
+	dff := &df.Fulfillment{
+		FulfillmentMessages: df.Messages{
+			{RichMessage: df.Text{Text: []string{out}}},
+			df.ForGoogle(df.SingleSimpleResponse(out, out)),
+		},
+	}
+
+	c.JSON(http.StatusOK, dff)
 
 	c.JSON(http.StatusOK, gin.H{})
 }
